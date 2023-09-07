bef314583065b3cde419032a3646ae1b60e22755
赵伟松 <zws2lll@gmail.com>
2023-09-07 18:10:57 +0800

    Update init.lua
    disable some extension providers

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- disable some extension providers
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
